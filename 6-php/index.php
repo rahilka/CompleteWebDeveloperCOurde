c0303db7bbdf5b674a68dc6a7bbfa515f590c77d
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-04-08 14:47:02 +0200

    GET variables

--- 6-php/index.php
+++ 6-php/index.php
@@ -1,20 +1,55 @@
 <?php
 
-$family = array("Rahilka", "Ljubica", "Ana", "Risto");
+  if (is_numeric($_GET) && $_GET > 0 && $_GET == round($_GET, 0)) {
 
-foreach ($family as $key => $value) {
+    $i = 2;
 
-  $family[$key] = $value." Simonov";
+    $isPrime = true;
 
-  echo "Array item ".$key."is ".$value."<br />";
+    while ($i < $_GET['number']) {
 
-}
+      if ($_GET['number'] % $i == 0) {
 
+        //number is not prime!
 
-for ($i = 0; $i < sizeof($family); $i++) {
+        $isPrime = false;
 
-  echo $family[$i]."<br>";
+      }
 
-}
+      $i++;
+
+    }
+
+    if ($isPrime) {
+
+      echo "<p>".$i." is a prime number</p>";
+
+    } else {
+
+      echo "<p>".$i." is not prime.</p>";
+
+    }
+
+  } else if($_GET) {
+
+    //user has submitted sth which is not a positive whole number
+
+    echo "<p>Please enter a positive whole number</p>";
+  }
+
+  //$_GET all the get variables'
 
  ?>
+
+
+ <p>
+   Please enter a whole number
+ </p>
+
+ <form>
+
+   <input name="number" type="text" />
+
+   <input type="submit" value="Go!" />
+
+ </form>
