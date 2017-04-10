6d0c7e07b14ff27838419b238e8ca745364848f3
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-04-10 20:19:00 +0200

    Retrieving data from a db

--- 7-mysql/index.php
+++ 7-mysql/index.php
@@ -1,14 +1,22 @@
 <?php
 
-  mysqli_connect("localhost", "db-user-name", "password")
+  $link =  mysqli_connect("localhost", "db-user-name", "password", "db-name");
 
   if(mysqli_connect_error()) {
 
-    echo "It wasn't connected successfully";
+    die ( "It wasn't connected successfully"); //stops if there is no connection
 
-  } else {
+  }
 
-    echo "Successfully connected";
+  $query = "SELECT * FROM users";
+
+  if($result = mysqli_query($link, $query)) {
+
+      $row = mysqli_fetch_array($result);
+
+      // print_r($row);
+
+      echo "Your email is ".$row['email']." and your password is: ".$row['password'];
 
   }
 
