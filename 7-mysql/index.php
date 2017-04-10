b6fc7f86acfb1151bd2d5d45981f033e25d6a5f9
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-04-10 20:28:08 +0200

    Insert and update data

--- 7-mysql/index.php
+++ 7-mysql/index.php
@@ -8,6 +8,14 @@
 
   }
 
+  // $query = "INSERT INTO `users` (`email`, `password`) VALUES ('rahilka@yahoo.com', '1234')";
+
+  // $query = "UPDATE `users` SET email = 'ljubica@yahoo.com' WHERE id=1 LIMIT 1";
+
+  $query = "UPDATE `users` SET password='dsadsaf15fsda153f1ds53fs1df1sd31fs' where email='rahilka@yahoo.com' LIMIT 1";
+
+  mysqli_query($link, $query);
+
   $query = "SELECT * FROM users";
 
   if($result = mysqli_query($link, $query)) {
