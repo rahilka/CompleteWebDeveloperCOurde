f0b69fe7ef8c6de085a3bd8f01ebcdd5dc2088c0
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-06-02 13:05:47 +0200

    Storing passwords Securely

--- 7-mysql/index.php
+++ 7-mysql/index.php
@@ -1,12 +1,13 @@
 <? php
 
-  setcookie("costomerId", "1234", time() + 60 * 60 * 24); //the cookie will expire in a day
-  // setcookie("costomerId", "1234", time() + 60 * 60 * 24 * 365); //the cookie will expire in a year
+    $salt = "ifij43j5nbj234nb4k2";
 
-  $_COOKIT("customerId") = "test"; //change the value of the cookie
+    // echo md5("password"); //turns the password into long alphanumeric string
 
-  setcookie("customerId", "", time() - 60 * 60);  to remove a cookie, use some time in the past, here is one hour
+    // echo md5($salt."password");
 
-  echo $_COOKIE["customerId"];
+    $row['id'] = 73;
 
+    echo md5(md5($row['id'])."password");
+    
  ?>
